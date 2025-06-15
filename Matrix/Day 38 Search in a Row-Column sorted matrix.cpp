19858fb3a78734b52dc7775d8e966725e8e8f613
Himanshu Rohilla <rohillahimanshu42@gmail.com>
2025-06-15 16:07:29 +0530

    Create Day 38 Search in a Row-Column sorted matrix.cpp
    Search in a Row-Column sorted matrix
    Difficulty: EasyAccuracy: 41.62%Submissions: 178K+Points: 2Average Time: 15m
    Given a 2D integer matrix mat[][] of size n x m, where every row and column is sorted in increasing order and a number x, the task is to find whether element x is present in the matrix.
    
    Examples:
    
    Input: mat[][] = [[3, 30, 38],[20, 52, 54],[35, 60, 69]], x = 62
    Output: false
    Explanation: 62 is not present in the matrix, so output is false.
    Input: mat[][] = [[18, 21, 27],[38, 55, 67]], x = 55
    Output: true
    Explanation: 55 is present in the matrix.
    Input: mat[][] = [[1, 2, 3],[4, 5, 6],[7, 8, 9]], x = 3
    Output: true
    Explanation: 3 is present in the matrix.
    Constraints:
    1 <= n, m <=1000
    1 <= mat[i][j] <= 109
    1<= x <= 109

---
+++ Matrix/Day 38 Search in a Row-Column sorted matrix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    bool matSearch(vector<vector<int>> &mat, int x) {
+        // your code here
+        int n=mat.size(),m=mat[0].size();
+        int i=0,j=m-1;
+        while(i<n && j>=0){
+            if(mat[i][j]==x)
+            return 1;
+            else if(mat[i][j]<x)
+            i++;
+            else
+            j--;
+        }
+        return 0;
+    }
+};
