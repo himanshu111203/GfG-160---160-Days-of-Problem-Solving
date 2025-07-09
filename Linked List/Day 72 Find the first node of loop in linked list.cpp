e4b9ceed195c12ac4f6f72b35c382eeea3be6bf4
Himanshu Rohilla <rohillahimanshu42@gmail.com>
2025-07-09 23:49:53 +0530

    Create Day 72 Find the first node of loop in linked list.cpp
    Given a head of the singly linked list. If a loop is present in the list then return the first node of the loop else return NULL.
    
    Custom Input format:
    A head of a singly linked list and a pos (1-based index) which denotes the position of the node to which the last node points to. If pos = 0, it means the last node points to null, indicating there is no loop.
    
    Examples:
    
    Input:
     
    Output: 3
    Explanation: We can see that there exists a loop in the given linked list and the first node of the loop is 3.
    Input:
     
    Output: -1
    Explanation: No loop exists in the above linked list.So the output is -1.
    Constraints:
    1 <= no. of nodes <= 106
    1 <= node->data <= 106 
    
    Expected Complexities
    Time Complexity: O(n)
    Auxiliary Space: O(1)

---
+++ Linked List/Day 72 Find the first node of loop in linked list.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    Node* findFirstNode(Node* head) {
+        Node *slow=head,*fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+};
